4de7ff27b7d588fd5290aa7de15f82d1c70fd2af
WessCoby <cobygiven@gmail.com>
2020-02-02 17:16:06 +0000

    Updated variable reference

--- scripts/createDefaultConfig.sh
+++ scripts/createDefaultConfig.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
+port=$(node -e "console.log(process.env.PORT)")
+domain=$(node -e "console.log(process.env.AUTH0_DOMAIN)")
+client_id=$(node -e "console.log(process.env.AUTH0_CLIENT_ID)")
+client_secret=$(node -e "console.log(process.env.AUTH0_CLIENT_SECRET)")
+callback_url=$(node -e "console.log(process.env.CALLBACK_URL)")
+base_url=$(node -e "console.log(process.env.BASE_URL)")
+
+
 if [ ! -f config.js ]; then
   cat > config.js << EOF
 // read process.env.NODE_ENV to set values depending on environment
 module.exports = {
-  PORT: '${process.env.PORT}',
-  AUTH0_DOMAIN: '${process.env.AUTH0_DOMAIN}',
-  AUTH0_CLIENT_ID: '${process.env.AUTH0_CLIENT_ID}',
-  AUTH0_CLIENT_SECRET: '${process.env.AUTH0_CLIENT_SECRET}',
-  CALLBACK_URL: '${process.env.CALLBACK_URL}',
-  BASE_URL: '${process.env.BASE_URL}',
+  PORT: '${port}',
+  AUTH0_DOMAIN: '${domain}',
+  AUTH0_CLIENT_ID: '${client_id}',
+  AUTH0_CLIENT_SECRET: '${client_secret}',
+  CALLBACK_URL: '${callback_url}',
+  BASE_URL: '${base_url}',
 };
 EOF
 fi;
